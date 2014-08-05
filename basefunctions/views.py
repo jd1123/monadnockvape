d9dfc2c8b88732dd2cc61dec8baa407eefbc9838
user <johnnydiabetic@gmail.com>
2014-08-05 06:47:28 -0400

    Initial commit

---
+++ basefunctions/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+def root(request):
+    context = RequestContext(request)
+    return HttpResponse("Hello World!")

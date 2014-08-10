e3e653d8d9c692df28a75c4cbde496fd50aeae16
johnnydiabetic <johnnydiabetic@gmail.com>
2014-08-10 19:16:11 -0400

    Edited html and added a function that doesnt work
    * basefunctions.views.get_client_ip()

--- basefunctions/views.py
+++ basefunctions/views.py
@@ -11,7 +11,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 def root(request):
     context = RequestContext(request)
     context_dict = {}
-    print 'Incoming request from: ' + request.META['REMOTE_ADDR']
     return render_to_response('index.html', context_dict, context)
 
 
@@ -25,3 +24,12 @@ def contact(request):
     context = RequestContext(request)
     context_dict = {}
     return render_to_response('contact.html', context_dict, context)
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip

fec3658f2aeb99360255ff9abd16fcd0c093f813
user <johnnydiabetic@gmail.com>
2014-09-06 12:16:32 -0400

    Updates

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,7 +4,7 @@ from inventory import views
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.category_list, name='home'),
-    url(r'^(?P<category>[a-zA-Z_]+)/$', views.sub_category_list, name='subs'),
-    url(r'^(?P<category>[a-zA-Z_]+)/(?P<sub_category>[a-zA-Z_]+)/$', views.inv_item_list, name='items'),
+    url(r'^(?P<category>[a-zA-Z_-]+)/$', views.sub_category_list, name='subs'),
+    url(r'^(?P<category>[a-zA-Z_-]+)/(?P<sub_category>[a-zA-Z_-]+)/$', views.inv_item_list, name='items'),
     # url(r'^(?P<category>\S+)/a/$', views.inv_item_list, name='items'),
 )

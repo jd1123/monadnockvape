f7f2a0e1f9f0653e7f50c8b3fe7881d1214dcf9c
jd1123 <johnnydiabetic@gmail.com>
2015-04-19 11:06:55 -0400

    Customer import script added

---
+++ import_customers.py
@@ -0,0 +1,27 @@
+import csv
+from juiceprogram.models import Customer, new_customer
+
+
+def import_customers():
+    i = 0
+    with open('jp.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            print row
+            if i>0:
+                save_customer(row)
+            i+=1
+
+
+def save_customer(customer):
+    fname = customer[0]
+    lname = customer[1]
+
+    if customer[3] == '':
+        juices = 0
+    else:
+        juices = int(customer[3])
+
+    new_customer(fname, lname, juices, 0) 
+
+import_customers()

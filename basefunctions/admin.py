ed72bbdfd2095a3482c004fa35f20dc52a9de8bf
jd1123 <johnnydiabetic@gmail.com>
2015-05-03 09:32:56 -0400

    Started mosiac models

--- basefunctions/admin.py
+++ basefunctions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from basefunctions.models import IndexImage, IndexImageAdmin, IndexMosiac, IndexMosiacAdmin
 
 # Register your models here.
+admin.site.register(IndexImage, IndexImageAdmin)
+admin.site.register(IndexMosiac, IndexMosiacAdmin)
--- basefunctions/models.py
+++ basefunctions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+class IndexImage(models.Model):
+    image = models.ImageField(upload_to = "static/img/")
+    caption = models.CharField(max_length = 40)
+
+class IndexImageAdmin(admin.ModelAdmin):
+    list_display = ['image', 'caption']
+
+
+class IndexMosiac(models.Model):
+    image1 = models.ForeignKey(IndexImage)
+
+class IndexMosiacAdmin(admin.ModelAdmin):
+    list_display = ['image1']
